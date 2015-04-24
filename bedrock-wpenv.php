6aff40c9787c26b3c70e21e78d2bee4787013bc9
Evan Mattson <me@aaemnnost.tv>
2015-04-24 17:34:27 -0400

    load autoloader

--- bedrock-wpenv.php
+++ bedrock-wpenv.php
@@ -5,6 +5,9 @@
  * Version: 1.0
  */
 
+// load classes whether installed as a package or standalone
+$autoloader = __DIR__ . '/vendor/autoload.php';
+if ( file_exists($autoloader) ) require $autoloader;
 
 /*
  * Initialize and activate any registered loaders
